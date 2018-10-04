20b21b94c86fb7045d6234280c8b227b8676375e
shrey183 <shrey183@gmail.com>
2018-10-05 01:11:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,34 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  4 23:20:33 2018
 
+@author: shrey.aryan
+"""
+import setuptools
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 
 def read(filename):
     with open(os.path.join(BASE_DIR, filename)) as file:
         return file.read()
 
+setuptools.setup(
+    name="sdk-python",
+    version="0.0.1",
+    author="SuperMario",
+    author_email="email@example.com",
+    description='Client-Server SDK for LiveScore',
+    long_description=read('README.rst'),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Livescore-api/sdk-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
 
-setup(name='livescore_client',
-      version='0.1',
-      description='Client-Server SDK for LiveScore',
-      long_description= read('README.rst'),
-      classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6.1',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content
-      ],
-      keywords='livescore api',
-      url='https://github.com/Livescore-api/sdk-python',
-      author='SuperMario',
-      author_email='will_add_something__here@gmail.com',
-      license='MIT',
-      packages=find_packages(),
-      install_requires=[
-          'markdown',
-      ],
-      include_package_data=True,
-      zip_safe=False)
