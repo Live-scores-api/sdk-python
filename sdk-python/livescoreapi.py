6068ae8a9b5d2e146a94b04689f111ac55ba3231
Shrey Aryan <shrey183@gmail.com>
2018-11-15 12:03:17 +0100

    Worked on Feedback_2

--- sdk-python/livescoreapi.py
+++ sdk-python/livescoreapi.py
@@ -6,6 +6,7 @@ Created on Sat Oct  6 21:39:23 2018
 @author: shrey.aryan
 """
 
+from urllib.parse import urlencode
 import urllib.request
 import json
 
@@ -28,26 +29,31 @@ class livescores:
     get_all()
     
     """
-    link_get_all = "http://livescore-api.com/api-client/scores/live.json?key={}&secret={}"
+    parent_link = "http://livescore-api.com/api-client"
+
    
     def __init__(self, key, secret):
+        key.strip()
+        secret.strip()
         
-        try: 
-            key.strip()
-            secret.strip()
-            assert len(key) == 16
-            assert len(secret) == 32
-            self.key = key
-            self.secret = secret 
-            
-        except Exception as e:
-            print("Key or Secret is Empty!")
+        if len(key) == 16:
+            raise Exception('length of key should not be different from 16. The current length of key is: {}'.format(len(key)))
+        if len(secret) == 32:
+            raise Exception('length of secret should not be different from 32. The current length of secret is: {}'.format(len(secret)))
+
+        self.key = key
+        self.secret = secret 
+        
+
+    def get_auth(self):
+        return urlencode({'key': self.key, 'secret': self.secret})
+        
     
-    def _make_api_call(self, link):
+    def _make_api_call(self, endpoint):
         """returns the source code given a link"""
-    
+        
         try:
-            http_response_object = urllib.request.urlopen(link)
+            http_response_object = urllib.request.urlopen(parent_link + endpoint +  '?' + urlli + self.get_auth())
             if http_response_object is not None: 
                 json_string = http_response_object.read().decode('utf-8')
                 json_dic = json.loads(json_string)
@@ -64,5 +70,5 @@ class livescores:
             
     def get_all(self):
         """returns all the livescores"""
-        link = self.link_get_all.format(self.key,self.secret)
-        return self._make_api_call(link)
+        endpoint = "/scores/live.json"
+        return self._make_api_call(endpoint)
