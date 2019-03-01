fc2c5b7229b5ff33d73655d1b13f69cf6d206e4d
unknown <nichev.emil@gmail.com>
2019-03-01 13:25:13 +0100

    country/language

--- sdk-python/livescoreapi.py
+++ sdk-python/livescoreapi.py
@@ -25,8 +25,11 @@ class Livescores:
        livescores = requests.get('{}scores/live.json?key={}&secret={}'.format(self.api_url, self.api_key, self.api_secret))
        return livescores.json()
     
+    def get_livescores_by_country(self, country_id):
+        country_livescores = requests.get('{}scores/live.json?key={}&secret={}&country={}'.format(self.api_url, self.api_key, self.api_secret, country_id))
+        return country_livescores.json()     
 
-        
-                
-    
 
+    def get_livescores_by_league(self, league_id, language_id):
+        league_livescores = requests.get('{}scores/live.json?key={}&secret={}&league={}&lang={}'.format(self.api_url, self.api_key, self.api_secret, league_id, language_id))
+        return league_livescores.json() 
--- sdk-python/test.py
+++ sdk-python/test.py
@@ -9,5 +9,25 @@ def livescores_api():
     #print(livescores_data)
     return livescores_data
 
-r=livescores_api()
-print(r)
+def by_country():
+    client = livescoreapi.Livescores('http://livescore-api.com/api-client/', '6r94GgdPiJ5ciqdx', '70Qx0KjZN2uD6jfdLohrFuhXem9wNm4U')
+    country_livescores_data=client.get_livescores_by_country(71)
+    return country_livescores_data
+
+def by_league(league_id, language_id):
+    client = livescoreapi.Livescores('http://livescore-api.com/api-client/', '6r94GgdPiJ5ciqdx', '70Qx0KjZN2uD6jfdLohrFuhXem9wNm4U')
+    if language_id == "ar" or language_id == "fa" or language_id == "en" or language_id == "ru":
+        league_livescores_data=client.get_livescores_by_league(league_id, language_id)
+        return league_livescores_data
+    else:
+        language_id == "en"
+        league_livescores_data=client.get_livescores_by_league(league_id, language_id)
+        print("The lnguage was changed to English")
+        return league_livescores_data
+
+#r=livescores_api()
+#print(r)
+cntry=by_country()
+print(cntry)
+liga=by_league(218, "bg")
+print(liga)
