811c02de5299d16d7ab3cf61aa80c9d7e389d431
unknown <nichev.emil@gmail.com>
2019-03-01 15:31:13 +0100

    checkings

--- sdk-python/livescoreapi.py
+++ sdk-python/livescoreapi.py
@@ -25,11 +25,37 @@ class Livescores:
        livescores = requests.get('{}scores/live.json?key={}&secret={}'.format(self.api_url, self.api_key, self.api_secret))
        return livescores.json()
     
-    def get_livescores_by_country(self, country_id):
-        country_livescores = requests.get('{}scores/live.json?key={}&secret={}&country={}'.format(self.api_url, self.api_key, self.api_secret, country_id))
-        return country_livescores.json()     
-
+    def get_livescores_by_country(self, country_id, language_id):
+        if country_id > 0:
+            country_livescores = requests.get('{}scores/live.json?key={}&secret={}&country={}&lang={}'.format(self.api_url, self.api_key, self.api_secret, country_id, language_id))
+            if language_id == "":
+                country_livescores = requests.get('{}scores/live.json?key={}&secret={}&country={}'.format(self.api_url, self.api_key, self.api_secret, country_id))
+                return country_livescores.json()
+            else:
+                country_livescores = requests.get('{}scores/live.json?key={}&secret={}&country={}&lang={}'.format(self.api_url, self.api_key, self.api_secret, country_id, language_id))
+                if language_id == "ar" or language_id == "fa" or language_id == "en" or language_id == "ru":
+                    return country_livescores.json()
+                else:
+                    language_id == "en"
+                    print("The lnguage was changed to English")    
+                    return country_livescores.json()
+        else:
+            print("Wrong country ID!")
+        
 
     def get_livescores_by_league(self, league_id, language_id):
-        league_livescores = requests.get('{}scores/live.json?key={}&secret={}&league={}&lang={}'.format(self.api_url, self.api_key, self.api_secret, league_id, language_id))
-        return league_livescores.json() 
+        if league_id > 0:
+            league_livescores = requests.get('{}scores/live.json?key={}&secret={}&league={}&lang{}'.format(self.api_url, self.api_key, self.api_secret, league_id, language_id))
+            if language_id == "":
+                league_livescores = requests.get('{}scores/live.json?key={}&secret={}&league={}'.format(self.api_url, self.api_key, self.api_secret, league_id))
+                return league_livescores.json() 
+            else:
+                league_livescores = requests.get('{}scores/live.json?key={}&secret={}&league={}&lang{}'.format(self.api_url, self.api_key, self.api_secret, league_id, language_id))
+                if language_id == "ar" or language_id == "fa" or language_id == "en" or language_id == "ru":
+                    return league_livescores.json()
+                else:
+                    language_id == "en"
+                    print("The lnguage was changed to English")    
+                    return league_livescores.json()
+        else:
+            print("Wrong league ID!")
--- sdk-python/test.py
+++ sdk-python/test.py
@@ -2,32 +2,31 @@ import requests
 import json
 import livescoreapi
 
+api_url = 'http://livescore-api.com/api-client/'
+api_key = ''
+api_secret = ''
 
-def livescores_api():
-    client = livescoreapi.Livescores('http://livescore-api.com/api-client/', '6r94GgdPiJ5ciqdx', '70Qx0KjZN2uD6jfdLohrFuhXem9wNm4U')
+
+def livescores_api(api_url,api_key,api_secret):
+    client = livescoreapi.Livescores(api_url,api_key,api_secret)
     livescores_data = client.get_all_livescores()
     #print(livescores_data)
     return livescores_data
 
-def by_country():
-    client = livescoreapi.Livescores('http://livescore-api.com/api-client/', '6r94GgdPiJ5ciqdx', '70Qx0KjZN2uD6jfdLohrFuhXem9wNm4U')
-    country_livescores_data=client.get_livescores_by_country(71)
+def by_country(api_url,api_key,api_secret, country_id, language_id):
+    client = livescoreapi.Livescores(api_url,api_key,api_secret)
+    country_livescores_data=client.get_livescores_by_country(country_id, language_id)
     return country_livescores_data
 
-def by_league(league_id, language_id):
-    client = livescoreapi.Livescores('http://livescore-api.com/api-client/', '6r94GgdPiJ5ciqdx', '70Qx0KjZN2uD6jfdLohrFuhXem9wNm4U')
-    if language_id == "ar" or language_id == "fa" or language_id == "en" or language_id == "ru":
-        league_livescores_data=client.get_livescores_by_league(league_id, language_id)
-        return league_livescores_data
-    else:
-        language_id == "en"
-        league_livescores_data=client.get_livescores_by_league(league_id, language_id)
-        print("The lnguage was changed to English")
-        return league_livescores_data
+def by_league(api_url,api_key,api_secret, league_id, language_id):
+    client = livescoreapi.Livescores(api_url,api_key,api_secret)
+    league_livescores_data=client.get_livescores_by_league(league_id, language_id)
+    return league_livescores_data
+   
 
-#r=livescores_api()
+#r=livescores_api(api_url,api_key,api_secret)
 #print(r)
-cntry=by_country()
+cntry=by_country(api_url,api_key,api_secret, 12, "fa")
 print(cntry)
-liga=by_league(218, "bg")
+liga=by_league(api_url,api_key,api_secret, 38, "bg")
 print(liga)
