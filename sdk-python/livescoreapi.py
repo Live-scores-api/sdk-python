b0149b8ee4073271297cfbe036370ca3d100ad81
unknown <nichev.emil@gmail.com>
2019-03-05 15:42:46 +0100

    Added api_key, secret and url validation + language(__init__)

--- sdk-python/livescoreapi.py
+++ sdk-python/livescoreapi.py
@@ -1,58 +1,66 @@
 import urllib.request
 import requests
 import re
+import datetime
+from urllib.parse import urlparse
 
 
-def validate_key(api_key):
-        if api_key is None:
-            raise ValueError("Missing API Key!")
-        if re.match("^[A-Za-z0-9_-]*$", api_key) and len(api_key) == 16:
-            print("API key is according to requirements")
+class Livescores: 
+
+    def validate_key(self, api_key):
+        if not api_key:
+            raise ValueError("None API Key!")
+        if re.match("^[A-Za-z0-9]*$", api_key) and len(api_key) == 16:
+            return True
         else:
             raise ValueError("Wrong API Key!")
     
 
-def validate_secret(api_secret):
-    if api_secret is None:
-        raise ValueError("Missing API Secret!")
-    if re.match("^[A-Za-z0-9_-]*$", api_secret) and len(api_secret) == 32:
-        print("API secret is according to requirements")
-    else:
-        raise ValueError("Wrong API Secret!")
+    def validate_secret(self, api_secret):
+        if not api_secret:
+            raise ValueError("None API Secret!")
+        if re.match("^[A-Za-z0-9]*$", api_secret) and len(api_secret) == 32:
+            return True
+        else:
+            raise ValueError("Wrong API Secret!")
 
-
-class Livescores: 
-    
+    def validate_url(self, api_url):
+        if not api_url:
+            raise ValueError("None API URL!")
+        
+       
     
     language_list = ['en', 'ar', 'ru', 'fa']
     api_url = 'http://livescore-api.com/api-client/'
-    #print(urllib.request.urlopen(api_url).getcode())
     api_key = '5555YYYYgggg21aD'
-    if api_key is not None:
-        validate_key(api_key)
     api_secret = '5555YYYYgggg21aDKKKK2222ssssYYYY'
-    if api_secret is not None:
-        validate_secret(api_secret)
+    
       
 
-    def __init__(self, api_url, api_key, api_secret, lang):
+    def __init__(self, api_url, api_key, api_secret, language='en'):
         self.api_url = api_url
+        if api_url is not None:
+            self.validate_url(api_url)
         self.api_key = api_key
+        if api_key is not None:
+            self.validate_key(api_key)
         self.api_secret = api_secret
-        lang_check = self.validate_language(lang)
+        if api_secret is not None:
+            self.validate_secret(api_secret)
+        lang_check = self.validate_language(language)
         if lang_check is True:
-            self.lang = lang
+            self.lang = language
 
 
     
-    def get_all_livescores(self, country_id, language_id, league_id):
+    def get_all_livescores(self, country_id, league_id):
         url = '{}scores/live.json?key={}&secret={}'.format(self.api_url, self.api_key, self.api_secret)
         if country_id is not None:
             self.validate_country(country_id)
             url = url + '&country=' + str(country_id)
-        if language_id is not None:
-            self.validate_language(language_id)
-            url = url + '&lang=' + language_id
+        #if language_id is not None:
+        #    self.validate_language(language_id)
+         #   url = url + '&lang=' + language_id
         if league_id is not None:
             self.validate_league(league_id)
             url = url + '&league=' + str(league_id)
@@ -87,22 +95,32 @@ class Livescores:
             raise ValueError("Wrong league ID")
 
 
-    def get_livescores_by_country(self, country_id, language_id):
-        return self.get_all_livescores(country_id, language_id, None)
+    def get_livescores_by_country(self, country_id):
+        return self.get_all_livescores(country_id, None)
 
                 
-    def get_livescores_by_league(self, league_id, language_id):
-        return self.get_all_livescores(None, language_id, league_id)
+    def get_livescores_by_league(self, league_id):
+        return self.get_all_livescores(None, league_id)
 
-    '''def get_fixtrures(self, country_id, league_id, language_id, date):
+
+    def get_all_fixtrures(self, country_id, league_id, date):
+        url = '{}fixtures/matches.json?key={}&secret={}'.format(self.api_url, self.api_key, self.api_secret)
         if country_id is not None:
             self.validate_country(country_id)
-            url = url + '&country_id=' + country_id
+            url = url + '&country_id=' + str(country_id)
+        if league_id is not None:
+            self.validate_league(league_id)
+            url = url + '&league=' + str(league_id)
+        #if language_id is not None:
+         #   self.validate_language(language_id)
+          #  url = url + '&lang=' + language_id
+        fixtures = requests.get(url)
+        return fixtures.json()
 
-        return []
 
-    def get_fixtrures_by_country(self, country_id, language_id):
-        return self.get_fixtrures(country_id, None, language_id, None)
+    def get_fixtrures_by_country(self, country_id):
+        return self.get_all_fixtrures(country_id, None, None)
+
 
     def get_todays_fixtures(self):
-        return self.get_fixtrures(None, None, None, today())'''
+        return self.get_all_fixtrures(None, None, date)
