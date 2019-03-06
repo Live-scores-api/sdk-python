62d3a74342bfffd849fac33874a4fee550aab19b
unknown <nichev.emil@gmail.com>
2019-03-06 13:48:34 +0100

    added validation for url and date format, and fixed None

--- sdk/livescoreapi.py
+++ sdk/livescoreapi.py
@@ -27,75 +27,103 @@ class LivescoresAPI:
 
     @staticmethod
     def validate_key(api_key):
+
+        if api_key is None:
+            raise ValueError("API Key must be defined!")
+
         if not (isinstance(api_key, str)):
             raise ValueError("API Key must be a string")
-        if not api_key:
-            raise ValueError("API Key cannot be None!")
+        
         if not(re.match("^[A-Za-z0-9]*$", api_key)) or len(api_key) != 16:
             raise ValueError("Invalid API Key format!")
         
 
     @staticmethod
     def validate_secret(api_secret):
+
+        if api_secret is None:
+            raise ValueError("API Secret must be defined!")
+
         if not (isinstance(api_secret, str)):
             raise ValueError("API Secret must be a string") 
-        if not api_secret:
-            raise ValueError("API Secret cannot be None!")
+
         if not(re.match("^[A-Za-z0-9]*$", api_secret)) or len(api_secret) != 32:
             raise ValueError("Invalid API Secret format!") 
 
 
     @staticmethod
     def validate_url(api_url):
+
+        if api_url is None:
+            raise ValueError("API URL must be defined!")
+
         if not (isinstance(api_url, str)):
             raise ValueError("API URL must be a string")
-        if not api_url:
-            raise ValueError("API URL cannot be None!")
-    
+
+        request = requests.get('http://livescore-api.com')
+        if not request.status_code == 200:
+             raise ValueError("API URL does not exist")
+        
     
     def get_all_livescores(self, country_id, league_id):
         url = '{}scores/live.json?key={}&secret={}&lang=en'.format(self.api_url, self.api_key, self.api_secret)
+
         if country_id is not None:
             self.validate_country(country_id)
             url = url + '&country=' + str(country_id)
+
         if league_id is not None:
             self.validate_league(league_id)
             url = url + '&league=' + str(league_id)
+
         livescores = requests.get(url)
         return livescores.json()
 
 
     def validate_country(self, country_id):
-        if not country_id:
+
+        if country_id is None:
             raise ValueError("Country ID must be defined")
+            
         if not (isinstance(country_id, int)):
-            raise ValueError("Country ID must be a integer")
-        if country_id <= 0:
+            raise ValueError("Country ID must be an integer")
+
+        if not country_id > 0:
             raise ValueError("Country ID must be a positive number")
         
        
     def validate_language(self, language_id): 
-        if not language_id:
-             raise ValueError("Language ID must be defined")
+
+        if language_id is None:
+            raise ValueError("Language ID must be defined")
+
         if not (isinstance(language_id, str)):
             raise ValueError("Language ID must be a string")
-        if language_id in self.language_list:
-            return True
+
+        if not language_id in self.language_list:
+            raise ValueError("Language ID is not supported")
 
 
     def validate_league(self, league_id):
-        if not league_id:
+
+        if league_id is None:
             raise ValueError("League ID must be defined")
+
         if not (isinstance(league_id, int)):
             raise ValueError("League ID must be a integer")
-        if league_id <= 0:
+
+        if not league_id > 0:
             raise ValueError("League ID must be a positive number")
 
 
     def validate_date(self, date):
-        if not date:
+
+        if date is None:
             raise ValueError('Date must be defined')
-        
+
+        if not datetime.datetime.strptime(date, '%Y-%m-%d'):
+            raise ValueError('Invalid date format')
+
 
     def get_livescores_by_country(self, country_id):
         return self.get_all_livescores(country_id, None)
@@ -107,12 +135,15 @@ class LivescoresAPI:
 
     def get_all_fixtures(self, country_id, league_id, date):
         url = '{}fixtures/matches.json?key={}&secret={}'.format(self.api_url, self.api_key, self.api_secret)
+
         if country_id is not None:
             self.validate_country(country_id)
             url = url + '&country_id=' + str(country_id)
+
         if league_id is not None:
             self.validate_league(league_id)
             url = url + '&league=' + str(league_id)
+
         if date is not None:
             self.validate_date(date)
             url = url + '&date=' + str(date)
